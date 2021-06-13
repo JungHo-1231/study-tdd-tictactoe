017da6c47e1c77ae75783afb0c807ca20105adb2
jung <jungpark007@naver.com>
2021-06-13 21:36:40 +0900

    04-draw

--- src/test/java/ch03/TicTacToe.java
+++ src/test/java/ch03/TicTacToe.java
@@ -5,20 +5,85 @@ public class TicTacToe {
     boolean playerTurn = true;
     private Character[][] board = {{'\0', '\0', '\0'},
             {'\0', '\0', '\0'}, {'\0', '\0', '\0'}};
+    private char lastPlayer = '\0';
+    private final int SIZE = 3;
 
-    public void play(int x, int y) throws Exception {
+    public String play(int x, int y) throws Exception {
         // x 조건
         checkValue(x, "X값이 올바르지 않습니다");
         // y 조건
         checkValue(y, "Y값이 올바르지 않습니다");
         // 올바른 곳에 두었는지 확인
         putBoard(x, y);
+        // 플레이어의 순서를 변경
+//        changePlayerTurn();
 
-        changePlayerTurn();
+        lastPlayer = nextPlayer();
+
+        if (isWin()) {
+            return lastPlayer + " is the winner";
+        } else if(isDraw()){
+            return "The result is draw";
+        } else {
+            return "No winner";
+        }
+    }
+
+    private boolean isDraw() {
+        for (int x = 0; x < SIZE; x++) {
+            for (int y = 0; y < SIZE; y++) {
+                if (board[x][y] == '\0') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean isWin() {
+        for (int i = 0; i < SIZE; i++) {
+
+            // 디버깅할때 값 확인용용
+            int boardResult = board[0][i] + board[1][i] + board[2][i];
+            int checkResultSize = lastPlayer * SIZE;
+
+            if (boardResult == checkResultSize) {
+                return true;
+            }
+
+            if ((board[0][0] + board[1][1] + board[2][2])
+                    == boardResult) {
+                return true;
+
+            } else if (boardResult == (board[0][2] + board[1][1] + board[2][0])) {
+
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isWin(int x, int y) {
+        int playerTotal = lastPlayer * 3;
+        char horizontal, vertical, diagonal1, diagonal2;
+        horizontal = vertical = diagonal1 = diagonal2 = '\0';
+        for (int i = 0; i < SIZE; i++) {
+            horizontal += board[i][y - 1];
+            vertical += board[x - 1][i];
+            diagonal1 += board[i][i];
+            diagonal2 += board[i][SIZE - i - 1];
+        }
+        if (horizontal == playerTotal
+                || vertical == playerTotal
+                || diagonal1 == playerTotal
+                || diagonal2 == playerTotal) {
+            return true;
+        }
+        return false;
     }
 
     private void changePlayerTurn() {
-        if (playerTurn){
+        if (playerTurn) {
             playerTurn = false;
         } else {
             playerTurn = true;
@@ -40,10 +105,17 @@ public class TicTacToe {
     }
 
     public char nextPlayer() {
-        if (playerTurn){
-            return 'X';
-        } else {
+        if (lastPlayer == 'X') {
             return 'O';
         }
+        return 'X';
     }
+
+//    public char nextPlayer() {
+//        if (playerTurn) {
+//            return 'X';
+//        } else {
+//            return 'O';
+//        }
+//    }
 }
--- src/test/java/ch03/TicTacToeSpec.java
+++ src/test/java/ch03/TicTacToeSpec.java
@@ -23,7 +23,6 @@ public class TicTacToeSpec {
 
     @Test
     void 같은자리에_한번_이상_두게되면_예외발생() throws Exception {
-
         ticTacToe.play(2,1);
 
         assertThrows(Exception.class, () ->{
@@ -43,6 +42,55 @@ public class TicTacToeSpec {
         assertEquals('O',ticTacToe.nextPlayer());
     }
 
+    @Test
+    void 아직_이긴사람이_없는_경우() throws Exception {
+        String actual = ticTacToe.play(1,1);
+        assertEquals("No winner", actual);
+    }
 
+    @Test
+    void 수직으로_이긴_경우() throws Exception {
+        ticTacToe.play(1, 1); // X
+        ticTacToe.play(1, 2); // O
+        ticTacToe.play(2, 1); // X
+        ticTacToe.play(2, 2); // O
+        String actual = ticTacToe.play(3, 1); // X
+        assertEquals("X is the winner", actual);
+    }
+
+    @Test
+    void 수평으로_이긴_경우() throws Exception {
+        ticTacToe.play(2, 1); // X
+        ticTacToe.play(1, 1); // O
+        ticTacToe.play(3, 1); // X
+        ticTacToe.play(1, 2); // O
+        ticTacToe.play(2, 2); // X
+        String actual = ticTacToe.play(1, 3); // O
+        assertEquals("O is the winner", actual);
+    }
+
+    @Test
+    public void whenPlayAndBottomTopDiagonalLineThenWinner() throws Exception {
+        ticTacToe.play(1, 3); // X
+        ticTacToe.play(1, 1); // O
+        ticTacToe.play(2, 2); // X
+        ticTacToe.play(1, 2); // O
+        String actual = ticTacToe.play(3, 1); // O
+        assertEquals("X is the winner", actual);
+    }
+
+//    @Test
+//    public void whenAllBoxesAreFilledThenDraw() throws Exception {
+//        ticTacToe.play(1, 1);
+//        ticTacToe.play(1, 2);
+//        ticTacToe.play(1, 3);
+//        ticTacToe.play(2, 1);
+//        ticTacToe.play(2, 3);
+//        ticTacToe.play(2, 2);
+//        ticTacToe.play(3, 1);
+//        ticTacToe.play(3, 3);
+//        String actual = ticTacToe.play(3, 2);
+//        assertEquals("The result is draw", actual);
+//    }
 
 }
